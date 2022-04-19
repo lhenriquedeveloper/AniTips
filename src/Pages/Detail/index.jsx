198af66fa44c2bbf1cdc16625ac515406e8a2477
Luis Henrique Sousa <sousarodriguesluishenrique@gmail.com>
2022-04-19 10:57:51 -0300

    Still working on Details, made some changes

--- src/Pages/Detail/index.jsx
+++ src/Pages/Detail/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import api from "../../Services/api";
+import "../../Styles/css/animeDetail.css";
 
 
 export default function Detail() {
@@ -31,39 +32,43 @@ export default function Detail() {
         console.log(uniqueAnime)
     }, [uniqueAnime])
 
+
     if (loading) {
         return (
-            <div className="filme-loading">
-                <h1>Carregando seu filme...</h1>
+            <div className="anime-loading">
+                <h1>Loading your anime...</h1>
             </div>
         );
     }
 
     return (
         <div>
+
             <div className="anime-info">
-
                 <h1>{uniqueAnime.titles.rj}</h1>
-                <img src={uniqueAnime.cover_image} alt={uniqueAnime.titles.rj} />
+                <div className="i-box">
+                    <img src={uniqueAnime.cover_image} alt={uniqueAnime.titles.rj} />
+                    <iframe src={uniqueAnime.trailer_url} frameBorder="0"></iframe>
+                </div>
                 <h3>Synopsis:</h3>
-                <p>{uniqueAnime.descriptions.rj}</p>
-                <p>Year: {uniqueAnime.season_year}</p>
-                <p>Number of Episodes: {uniqueAnime.episodes_count}</p>
-                <p>Genres: {uniqueAnime.genres}</p>
-                <p>Score: {uniqueAnime.score}</p>
-
+                <p>{uniqueAnime.descriptions.en.toString()}</p>
+                <p><span>Year:</span> {uniqueAnime.season_year}</p>
+                <p><span>Number of Episodes:</span> {uniqueAnime.episodes_count}</p>
+                <p><span>Genres:</span> {uniqueAnime.genres.toString()}</p>
+                <p><span>Score:</span> {uniqueAnime.score}</p>
                 <div className="botoes">
                     <button onClick={() => { }}> Save Anime </button>
                     <button>
                         <a
                             target="blank"
-                        // href={`https://www.youtube.com/results?search_query= ${uniqueAnime.titles.en} Trailer`}
+                            href={`https://www.youtube.com/results?search_query= ${uniqueAnime.titles.rj} Trailer`}
                         >
-                            Trailer
+                            Search Trailer on Youtube
                         </a>
                     </button>
                 </div>
             </div>
+
         </div>
     )
 }

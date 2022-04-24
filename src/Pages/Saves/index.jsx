d8ddb1622c321be44094d0db093f18e5b8094f40
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-04-24 15:16:50 -0300

    Sync Projects

--- src/Pages/Saves/index.jsx
+++ src/Pages/Saves/index.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+import firebase from "../../Services/firebaseconnection";
+
+const [animeSave, setAnimeSave] = useState([]);
+
+
+useEffect(() => {
+
+    async function listSave() {
+        const user = firebase.auth().currentUser
+
+        await firebase
+            .firestore()
+            .collection("favorites")
+    }
+
+})
+
+
 export default function Saves() {
     return (
         <div>
-            <h1>In production</h1>
+
         </div>
     )
 }

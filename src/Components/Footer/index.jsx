57a6bb269cf494452ff26206ce016703180b7e66
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-04-27 18:37:48 -0300

    fix: change <Link/> to <a/>

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom"
-import "../../Styles/css/footerStyle.css"
-import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from "react-router-dom";
+import "../../Styles/css/footerStyle.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <nav className="navigationWrapper-footer">
-                <div className="logoWrapper-footer">
-                    <span className="stylish-footer">Made by <Link to="https://github.com/lhenriquedeveloper/AniTips">@LhenriqueDeveloper <GitHubIcon></GitHubIcon></Link></span>
-                </div>
-            </nav>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <nav className="navigationWrapper-footer">
+        <div className="logoWrapper-footer">
+          <span className="stylish-footer">
+            Made by{" "}
+            <a
+              href="https://github.com/lhenriquedeveloper/AniTips"
+              target="_blank"
+            >
+              @LhenriqueDeveloper <GitHubIcon></GitHubIcon>
+            </a>
+          </span>
+        </div>
+      </nav>
+    </footer>
+  );
 }
